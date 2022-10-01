5dee0cc94f9caad36d9f2a02faa121b2cf6c891d
AKJ <kjajila@gmail.com>
2022-10-01 19:09:00 -0400

    add validation function for user input

--- assets/script.js
+++ assets/script.js
@@ -23,37 +23,42 @@ function finalScore(){
 //checking user input includes r/p/s
 function isValid(inputFromUser){
      if(inputFromUser==="R" || inputFromUser==="P" || inputFromUser==="S"){
+          alert("testing");
           return true;
      }
      else{
-          return false;
+          startGame();
      }
 }
 
 //taking user input and checking it against computer's random selection
 function startGame(){
-     var userInput=prompt("Type 'r' for Rock:  Type 's' for scissors: Type 'p' for paper: Which one you want to select?").toUpperCase();
-     if(isValid(userInput)){
-          var rand=Math.floor(Math.random()*(option.length));
-          var computer=option[rand];
-          alert("Computer selected: "+computer);
-          if(userInput===computer){
-               scoreCard.tie++;
-               alert("It is a tie");
-          }
-          else if((userInput==='R' && computer==='S')||(userInput==='S' && computer==='P') ||(userInput==='P' && computer==='R')){
-               scoreCard.user++;
-               alert("You WON!!!");
-          }
-          else{
-               scoreCard.computer++;
-               alert("SORRY you lost");
-          }
-          finalScore();
-     }
-    else{
+     var userInput=prompt("Type 'r' for Rock:  Type 's' for scissors: Type 'p' for paper: Which one you want to select?");
+     if(userInput===null){
           startGame();
-    }    
+     }
+     else{
+          userInput=userInput.toUpperCase();
+          if(isValid(userInput)){
+               var rand=Math.floor(Math.random()*(option.length));
+               var computer=option[rand];
+               alert("Computer selected: "+computer);
+               if(userInput===computer){
+                    scoreCard.tie++;
+                    alert("It is a tie");
+               }
+               else if((userInput==='R' && computer==='S')||(userInput==='S' && computer==='P') ||(userInput==='P' && computer==='R')){
+                    scoreCard.user++;
+                    alert("You WON!!!");
+               }
+               else{
+                    scoreCard.computer++;
+                    alert("SORRY you lost");
+               }
+               finalScore();
+          }      
+          
+     }   
 }
 
 
