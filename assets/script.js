91ff83d6bd4e4626860fc7df257881fc7480a3b8
AKJ <kjajila@gmail.com>
2022-10-01 18:36:58 -0400

    add functions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-
-var option="rps";
+//global variables
+var option="RPS";
 var scoreCard={
      tie: 0,
      user: 0,
@@ -7,25 +7,8 @@ var scoreCard={
 };
 
 
-function startGame(){
-     var userInput=prompt("Type 'r' for Rock:  Type 's' for scissors: Type 'p' for paper: Which one you want to select?");
-     var rand=Math.floor(Math.random()*(option.length));
-     var computer=option[rand];
-     alert("Computer select: "+computer);
-     if(userInput===computer){
-          scoreCard.tie++;
-          //alert("tie"+scoreCard.tie);
-     }
-     else if((userInput==='r' && computer==='s')||(userInput==='s' && computer==='p') ||(userInput==='p' && computer==='r')){
-               scoreCard.user++;
-               //alert("user"+scoreCard.user);
-          }
-     else{
-          scoreCard.computer++;
-          //alert(scoreCard.computer);
-     }
-     finalScore();
-}
+//checking whether the player want to conitnue? If yes start the game again
+// else display the final scoreboard   
 function finalScore(){
      var cont=confirm("Do you want to continue?")
      if(cont)
@@ -36,16 +19,28 @@ function finalScore(){
           alert("This is the final score card: You won "+scoreCard.user+ " times and computer won "+scoreCard.computer+" and "+scoreCard.tie+" ties.");
      }
 }
-startGame();
 
-
-// if one is R and other is P. P win
-// if one is R and other is S. R win
-// if one is S and other is P. S win
-// display the message based on the rule
-// allow user to select whether he want to play again or not
-// if user want to continue restart the steps again
-// if user doesn't want to continue display a bye message
+//taking user input and checking it against computer's random selection
+function startGame(){
+     var userInput=prompt("Type 'r' for Rock:  Type 's' for scissors: Type 'p' for paper: Which one you want to select?").toUpperCase();
+     var rand=Math.floor(Math.random()*(option.length));
+     var computer=option[rand];
+     alert("Computer selected: "+computer);
+     if(userInput===computer){
+          scoreCard.tie++;
+          alert("It is a tie");
+     }
+     else if((userInput==='R' && computer==='S')||(userInput==='S' && computer==='P') ||(userInput==='P' && computer==='R')){
+          scoreCard.user++;
+          alert("You WON!!!");
+     }
+     else{
+          scoreCard.computer++;
+          alert("SORRY you lost");
+     }
+     finalScore();
+}
 
 
 //function call
+startGame();
